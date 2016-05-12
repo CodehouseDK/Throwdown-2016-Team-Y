f4ecf390b575260f85153c5233f9733f27a3dccc
Henrik Hjarding <gjerster@gmail.com>
2016-05-12 18:41:30 +0200

    removed js and js map

--- wwwroot/scripts/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var frokost = require("../scripts/ServiceLayer/FrokostService");
-require("../style/style.scss");
-var frokostService = new frokost.FrokostService();
-alert(frokostService.getFrokost());
-//# sourceMappingURL=app.js.map
