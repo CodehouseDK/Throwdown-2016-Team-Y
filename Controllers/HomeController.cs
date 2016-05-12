397ebedc679e73dfd1c165220fea297484f6addb
Dennis Nicolaj Biil <dnb@codehouse.com>
2016-05-12 11:34:10 +0200

    Db create

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNet.Mvc;
+using TeamY.Infrastructure;
 using TeamY.Models;
 using TeamY.Services;
 
@@ -7,12 +8,17 @@ namespace TeamY.Controllers
     public class HomeController : Controller
     {
         INameService _nameService;
-        public HomeController(INameService nameService){
-          _nameService = nameService;   
+        private TeamyDbContext _teamyDbContext;
+        public HomeController(INameService nameService, TeamyDbContext teamyDbContext)
+        {
+            _nameService = nameService;
+            _teamyDbContext = teamyDbContext;
         }
+
         public IActionResult Index()
         {
             var model = new HomeModel(_nameService.GetName());
+            var test = _teamyDbContext.Database.EnsureCreated();
             return View(model);
         }
 
---
+++ Infrastructure/TeamyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.Entity;
+
+namespace TeamY.Infrastructure
+{
+    public class TeamyDbContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
