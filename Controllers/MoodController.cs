a8f83ca272f4de64b02b637aed91271694b05f1f
Henrik Hjarding <gjerster@gmail.com>
2016-05-12 17:37:35 +0200

    Fix merge error

--- Controllers/MoodController.cs
+++ Controllers/MoodController.cs
@@ -1,51 +1,12 @@
-<<<<<<< ef259d2b2f57b37757416f2d60a62c337cb1f0a0
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNet.Mvc;
 using TeamY.Domain;
-=======
-﻿using System.Linq;
-using Microsoft.AspNet.Mvc;
->>>>>>> added basic mood ts
 using TeamY.Infrastructure;
 
 namespace TeamY.Controllers
 {
-<<<<<<< ef259d2b2f57b37757416f2d60a62c337cb1f0a0
-    public class MoodController
-    {
-        private TeamyDbContext _dbContext;
-
-        public MoodController(TeamyDbContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
-
-        [HttpGet]
-        public MoodRegistration GetLatestMoodRegistration()
-        {
-            return _dbContext.MoodRegistrations.FirstOrDefault();
-        }
-
-        [HttpGet]
-        public List<MoodRegistration> GetAllMoodRegistration()
-        {
-            return _dbContext.MoodRegistrations.ToList();
-        }
-        
-        [HttpGet]
-        public List<MoodRegistration> GetMoodRegistrationsByDate(string from, string to)
-        {
-            return _dbContext.MoodRegistrations.Where(x => x.Registered >= DateTime.Parse(from) && x.Registered >= DateTime.Parse(to)).ToList();
-        }
-
-
-
-
-    }
-}
-=======
     [Route("api/[controller]")]
     public class MoodController : Controller
     {
@@ -54,6 +15,24 @@ namespace TeamY.Controllers
         public MoodController(TeamyDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet]
+        public MoodRegistration GetLatestMoodRegistration()
+        {
+            return _context.MoodRegistrations.FirstOrDefault();
+        }
+
+        [HttpGet]
+        public List<MoodRegistration> GetAllMoodRegistration()
+        {
+            return _context.MoodRegistrations.ToList();
+        }
+
+        [HttpGet]
+        public List<MoodRegistration> GetMoodRegistrationsByDate(string from, string to)
+        {
+            return _context.MoodRegistrations.Where(x => x.Registered >= DateTime.Parse(from) && x.Registered >= DateTime.Parse(to)).ToList();
         }
 
         [HttpGet("{mood}")]
@@ -69,4 +48,3 @@ namespace TeamY.Controllers
         }
     }
 }
->>>>>>> added basic mood ts
